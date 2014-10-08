a8cbe76b69f3db094b2b660b0f93c7143dca4a7f
Andrew Popovich <andrewpop1118@hotmail.com>
2014-10-08 10:23:48 -0400

    Stubbed out Banker Class

--- src/Banker.java
+++ src/Banker.java
@@ -1,4 +1,28 @@
 
 public class Banker {
-
+	private int nUnits;
+	
+	public Banker(int nUnits) {
+		this.nUnits = nUnits;
+	}
+	
+	public void setClaim() {
+		
+	}
+	
+	public boolean request(int nUnits) {
+		return false;
+	}
+	
+	public void release(int nUnits) {
+		
+	}
+	
+	public int allocated() {
+		return 0;
+	}
+	
+	public int remaining() {
+		return 0;
+	}
 }
