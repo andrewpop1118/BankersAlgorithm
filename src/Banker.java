f8762878c9d6de6139d38580fc9023bccd7c85b9
Andrew Popovich <andrewpop1118@hotmail.com>
2014-10-08 10:45:58 -0400

    Implemented setClaim method

--- src/Banker.java
+++ src/Banker.java
@@ -1,13 +1,39 @@
+/**
+ * The Banker class handles and simulates resource allocation for each thread
+ * that requests a claim.  Deadlock is avoided in this class through the use
+ * of the Banker's Algorithm.
+ * 
+ * @author Andrew Popovich (ajp7560@rit.edu)
+ */
+
+import java.util.LinkedHashMap;
 
 public class Banker {
 	private int nUnits;
+	private LinkedHashMap<Thread,int[]> allocation; //May need to change
 	
 	public Banker(int nUnits) {
 		this.nUnits = nUnits;
 	}
 	
-	public void setClaim() {
+	public void setClaim(int nUnits) {
+		Thread claimingThread = Thread.currentThread();
 		
+		//Prevent race conditions for threads attempting to make requests
+		//at the same time
+		synchronized(this) {
+			if(allocation.get(claimingThread) != null ||
+			   nUnits < 0 ||
+			   nUnits < this.nUnits) {
+				System.exit(1); //Terminate abnormally in these cases
+			}
+			
+			int[] allocationArray = {0, nUnits};
+			allocation.put(claimingThread, allocationArray);
+			
+			System.out.println("Thread "+claimingThread.getName()+
+					" sets a claim for "+nUnits+" units.");
+		}
 	}
 	
 	public boolean request(int nUnits) {
