d3dad60842f1298eb58a87d1984cd42cc0c6c5c1
Justin Cotner <jdc9622@rit.edu>
2014-10-10 10:19:44 -0400

    Driver class finished

--- src/Driver.java
+++ src/Driver.java
@@ -9,21 +9,32 @@ import java.util.ArrayList;
 
 public class Driver {
 	
-	final private static int totalUnits = 5;
+	final private static int bankersResources = 4;
 	final private static int numClients = 2;
-	final private static int clientNUnits = 2;
+	final private static int clientNUnits = 3;
 	final private static int clientNRequests = 3;
+	final private static long minSleepMillis = 1000;
+	final private static long maxSleepMillis = 5000;
 	
 	public static void main(String[] args){
-		Banker banker = new Banker(totalUnits);
+		Banker banker = new Banker(bankersResources);
 		ArrayList<Client> clients = new ArrayList<Client>();
 		
 		for(int i = 1; i < numClients; i++){
-			clients.add(new Client(("Client " + i), banker, clientNUnits, clientNRequests, (long)1000, (long)3000));
+			clients.add(new Client(("Client " + i), banker, clientNUnits,
+					clientNRequests, minSleepMillis, maxSleepMillis));
 		}
 		
 		for(Client client: clients){
 			client.start();
+		}
+		
+		for(Client client: clients){
+			try{
+				client.join();
+			}
+			
+			catch(InterruptedException e){}
 		}
 	}
 	
